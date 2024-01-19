50cc477e29f4a6d9167e8dc284d38a9735122f7b
kindred008 <talia.preston123@gmail.com>
2024-01-19 18:07:42 +0000

    Ensured all current code follows coding convention

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -13,10 +13,10 @@ public class PlayerControl : MonoBehaviour
     private BoxCollider2D _boxCollider2d;
     private Rigidbody2D _playerRb;
 
-    private Vector2 moveInput;
+    private Vector2 _moveInput;
 
     [Header("Movement")]
-    [SerializeField] private float moveSpeed = 2;
+    [SerializeField] private float _moveSpeed = 2;
 
     private void Awake()
     {
@@ -26,17 +26,17 @@ public class PlayerControl : MonoBehaviour
 
         _playerInput.actions["Move"].performed += ctx =>
         {
-            moveInput = ctx.ReadValue<Vector2>();
+            _moveInput = ctx.ReadValue<Vector2>();
         };
         _playerInput.actions["Move"].canceled += ctx =>
         {
-            moveInput = ctx.ReadValue<Vector2>();
+            _moveInput = ctx.ReadValue<Vector2>();
         };
     }
 
     private void Update()
     {
-        var moveValue = moveInput * moveSpeed;
+        var moveValue = _moveInput * _moveSpeed;
 
         _playerRb.velocity = moveValue;
     }
