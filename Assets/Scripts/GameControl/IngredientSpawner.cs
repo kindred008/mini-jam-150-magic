6fef49f4b5cfcb357b4b48485dfdddac8c956e1f
kindred008 <talia.preston123@gmail.com>
2024-01-21 14:27:04 +0000

    Setup scene with current decorations

--- Assets/Scripts/GameControl/IngredientSpawner.cs
+++ Assets/Scripts/GameControl/IngredientSpawner.cs
@@ -26,21 +26,22 @@ public class IngredientSpawner : MonoBehaviour
 
         leftBound = bounds.center.x - bounds.max.x;
         rightBound = bounds.center.x + bounds.max.x;
-        topBound = bounds.center.y + bounds.max.y;
+        topBound = bounds.center.y + bounds.max.y + 1; // This +1 is a dumb fix because the game isn't a complete square
         bottomBound = bounds.center.y - bounds.max.y;
     }
 
     public void SpawnIngredient(IngredientsScriptableObject ingredient)
     {
-        var randomX = Random.Range(leftBound + 0.5f, rightBound - 0.5f);
-        var randomY = Random.Range(topBound - 1, bottomBound + 1);
+        var randomX = Random.Range(leftBound, rightBound);
+        var randomY = Random.Range(topBound, bottomBound);
 
         var spawnLocation = new Vector2(randomX, randomY);
         var spawnCell = _colliderTilemap.WorldToCell(spawnLocation);
 
         Collider2D[] colliders = Physics2D.OverlapBoxAll(spawnLocation, new Vector2(0.2f, 0.2f), 0f);
 
-        if (_colliderTilemap.HasTile(spawnCell) || colliders.Length > 0)
+        //if (_colliderTilemap.HasTile(spawnCell) || colliders.Length > 0)
+        if (colliders.Length > 0)
         {
             SpawnIngredient(ingredient);
         } else
